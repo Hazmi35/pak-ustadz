0a17355709f53665de0fd147c9771cc96d17376d
Hazmi35 <contact@hzmi.xyz>
2022-03-23 22:35:18 +0700

    feat(DaftarCommand): Create DaftarCommand (#4)

--- prisma/migrations/20220319134904_init/migration.sql
+++ prisma/migrations/20220319134904_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "userId" BIGINT NOT NULL,
+    "userId" TEXT NOT NULL,
     "daerah" TEXT NOT NULL
 );
