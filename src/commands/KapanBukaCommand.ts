70aee576dfea2181c3f4ff17b32248d15b9a7a3c
Hazmi Alfarizqi <contact@hzmi.xyz>
2022-04-01 22:27:26 +0700

    feat(KapanBukaCommand): Command KapanBuka

---
+++ src/commands/KapanBukaCommand.ts
@@ -0,0 +1,47 @@
+import { ApplicationCommandOptionChoice, AutocompleteInteraction, CommandInteraction } from "discord.js";
+import { BaseCommand } from "../structures/BaseCommand";
+import { PakUstadz } from "../structures/PakUstadz";
+
+export class KapanBukaCommand extends BaseCommand {
+    public constructor(pakUstadz: PakUstadz) {
+        super(pakUstadz);
+        this.meta
+            .setName("kapanbuka")
+            .setDescription("Kapan buka?")
+            .addStringOption(option => option
+                .setName("daerah")
+                .setAutocomplete(true)
+                .setDescription("Daerah yang dimaksud")
+                .setRequired(false));
+    }
+
+    public async execute(ctx: AutocompleteInteraction | CommandInteraction): Promise<void> {
+        if (ctx.isAutocomplete()) {
+            const search = ctx.options.getString("daerah")!.toLowerCase();
+            return ctx.respond(this.generateOptions()
+                .filter(o => o.name.toLowerCase().includes(search)).slice(0, 25));
+        }
+
+        const currentData = await this.pakUstadz.userData.findFirst({ where: { userId: ctx.user.id } });
+        const daerah = ctx.options.getString("daerah");
+        const daerahTujuan = currentData?.daerah ?? daerah;
+        const namaDaerah = this.generateOptions().find(i => i.value === daerahTujuan)!.name;
+
+        if (daerahTujuan === null) {
+            return ctx.reply("Sepertinya kamu belum mendaftarkan daerah mu kedalam database bot dan tidak menyebutkan daerah di command ini");
+        }
+
+        this.pakUstadz.imsakiyah.get(daerahTujuan);
+        const now = new Date(Date.now());
+        const today = this.pakUstadz.imsakiyah.get(daerahTujuan)!.find(i2 => i2.bulan === now.getMonth() && i2.tanggal === now.getDate());
+
+        if (!today) return ctx.reply("Hari ini bukan Puasa Ramadhan.");
+
+        return ctx.reply(`Maghrib di daerah \`${namaDaerah}\` adalah: <t:${today.maghrib.getTime() / 1000}:R> (<t:${today.maghrib.getTime() / 1000}:t>)`);
+    }
+
+    private generateOptions(): ApplicationCommandOptionChoice[] {
+        return Array.from(this.pakUstadz.imsakiyah.keys())
+            .map(i => ({ name: i.split("-").map(n => `${n.charAt(0).toUpperCase()}${n.slice(1)}`).join(" "), value: i }));
+    }
+}
